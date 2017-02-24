8c38b4959cd17e13875d6c27feed770345fd2624
Kyle Lin <kylejlin.us@gmail.com>
2017-02-23 23:31:29 -0800

    Update FractohRouter.js

--- src/FractohRouter.js
+++ src/FractohRouter.js
@@ -3,8 +3,32 @@ import FractohFlowTree from 'FractohFlowTree.js';
 class FractohRouter {
   constructor(view) {
     this.view = view;
-    this.location = '/';
+    this.view.setRouter(this);
+    
+    this.location = [];
     this.flowTree = FractohFlowTree;
+  }
+  
+  getOptions() {
+    let options = this.flowTree;
+    for (let path of this.location) {
+      options = options[path];
+    }
+    
+    return options;
+  }
+  
+  choose(path) {
+    const options = this.getOptions();
+    if (options[path] === '..') {
+      this.location.pop();
+      this.view.update();
+    } else if ('object' === typeof options[path]) {
+      this.location.push(path);
+      this.view.update();
+    } else {
+      options[path](this);
+    }
   }
 }
 
